0bb4e6f6ec4dda1ed9579e4e674f486ad5cafc82
JaPNaA <leonehuang100@gmail.com>
2021-08-12 23:39:03 -0400

    Add single-page site support to github4SpaceTabs

--- userscripts/github4SpaceTabs.user.js
+++ userscripts/github4SpaceTabs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github 4 space tabs
 // @namespace    https://japnaa.github.io/Userscripts/
-// @version      0.1
+// @version      0.2
 // @description  Sets the tab width to 4
 // @author       JaPNaA
 // @match        https://github.com/*
@@ -10,11 +10,44 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
+    const body = document.body;
+    let timeoutHandle = -1;
 
-    const elms = document.querySelectorAll("[data-tab-size]");
+    function updateElm() {
+        disconnect();
 
-    for (const elm of elms) {
-        elm.dataset.tabSize = "4";
+        const elms = document.querySelectorAll("[data-tab-size]");
+
+        for (const elm of elms) {
+            elm.dataset.tabSize = "4";
+        }
+
+        observe();
     }
+
+    function updateElmOnTimeout() {
+        clearTimeout(timeoutHandle);
+        timeoutHandle = setTimeout(function () {
+            updateElm();
+        }, 100);
+    }
+
+    const observer = new MutationObserver(function () {
+        updateElmOnTimeout();
+    });
+
+    function observe() {
+        observer.observe(body, {
+            childList: true,
+            subtree: true,
+            attributes: false
+        });
+    }
+
+    function disconnect() {
+        observer.disconnect();
+    }
+
+    updateElm();
+    observe();
 })();
